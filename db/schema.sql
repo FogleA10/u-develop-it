04634ad296e4b40fb6a527469e1064f690d5c81b
audreyfogle <FogleA10@gmail.com>
2022-06-02 22:59:46 -0400

    setting up

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCAR(30)NOT NULL,
+    industry_connected BOOLEAN NOT NULL
+);
+
+INSERT INTO candidates (industry_connected, last_name, first_name)
+VALUES INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butler', 1),
+  ('Unica', 'Zurn', 1);
+
+SELECT first_name, last_name FROM candidates;
